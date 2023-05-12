ebaefe1c65fb8db87a04b8453b9ce61ed68899b5
sayaka <sayaka.rinrin0723@gmail.com>
2023-05-12 22:22:35 +0900

    Initial Commit

---
+++ TechMon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechMon
+//
+//  Created by 大島彩也夏 on 2023/05/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
